babc11185d532609d3a511373c542c8ec066e8f0
jaemoon <woans5970@naver.com>
2026-01-23 22:42:45 +0900

    배너 주소 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-banner.png",
+        url: "https://jaemoon99.github.io/og-banner.png",
         width: 1200,
         height: 630,
         alt: "Jaemoon's Portfolio",
